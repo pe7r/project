d64cb183f554d0d9e51749b30593fc70e61c588a
Peter Yevtushenko <petrucho.u@gmail.com>
2019-03-05 10:50:48 +0200

    added Header component to App

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './Header.js';
 
 class App extends Component {
   render() {
     return (
       <div class="App">
-        <h1> TodoList </h1>
+        <Header />
 
-      <input type="text" />
-      <input type="submit" value="Add" onclick="" />
+        <input type="text" />
+        <input type="submit" value="Add" onclick="" />
 
-      <input type="submit" value="Check All" />
-      <input type="submit" value="Uncheck All" />
+        <input type="submit" value="Check All" />
+        <input type="submit" value="Uncheck All" />
 
-      <input type="checkbox" />
-      <input type="text" />
+        <input type="checkbox" />
+        <input type="text" />
       </div>
     );
   }
