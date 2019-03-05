bc06432194c505886952b36b0a337fb606d1a91b
Peter Yevtushenko <petrucho.u@gmail.com>
2019-03-05 11:10:37 +0200

    added AddTask component

---
+++ src/AddTask.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function AddTask() {
+	return (
+		<div>
+			<input type="text" placeholder="Type here..." />
+        	<input type="submit" value="Add" onclick="" />
+		</div>
+	)
+}
+
+export default AddTask;
--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './Header.js';
+import AddTask from './AddTask.js';
 
 class App extends Component {
   render() {
     return (
       <div class="App">
         <Header />
-
-        <input type="text" />
-        <input type="submit" value="Add" onclick="" />
+        <AddTask />
+        <TaskActions />
 
         <input type="submit" value="Check All" />
         <input type="submit" value="Uncheck All" />
