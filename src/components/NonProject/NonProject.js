ec0ae00c1b12079c9649747b85c0a9ca329c0527
Peter Yevtushenko <petrucho.u@gmail.com>
2019-03-07 10:31:02 +0200

    Redoing react func components into class components(March 7) #2

--- src/components/NonProject/NonProject.js
+++ src/components/NonProject/NonProject.js
@@ -2,40 +2,51 @@ import React from "react"
 import ReactDOM from "react-dom"
 
 // #1
-function App() {
+class App extends React.Component {
+  render() {
     return (
-        <div>
-            <Header />
-            <Greeting />
-        </div>
-    )
+          <div>
+              <Header />
+              <Greeting />
+          </div>
+      )
+  }
+    
 }
 
 // #2
-function Header(props) {
+class Header extends React.Component {
+  render() {
     return (
-        <header>
-            <p>Welcome, {props.username}!</p>
-        </header>
-    )
+          <header>
+              <p>Welcome, {this.props.username}!</p>
+          </header>
+      )
+  }
+
+    
 }
 
 // #3
-function Greeting() {
+class Greeting extends React.Component {
+  render() {
     const date = new Date()
-    const hours = date.getHours()
-    let timeOfDay
+      const hours = date.getHours()
+      let timeOfDay
+      
+      if (hours < 12) {
+          timeOfDay = "morning"
+      } else if (hours >= 12 && hours < 17) {
+          timeOfDay = "afternoon"
+      } else {
+          timeOfDay = "night"
+      }
+
+      return (
+          <h1>Good {timeOfDay} to you, sir or madam!</h1>
+      )
+  }
     
-    if (hours < 12) {
-        timeOfDay = "morning"
-    } else if (hours >= 12 && hours < 17) {
-        timeOfDay = "afternoon"
-    } else {
-        timeOfDay = "night"
-    }
-    return (
-        <h1>Good {timeOfDay} to you, sir or madam!</h1>
-    )
 }
 
 ReactDOM.render(<App />, document.getElementById("root"))
